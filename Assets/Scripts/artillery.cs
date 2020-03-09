c5c53745712d09c2f968d4f88febfba7986f8a11
tanakaXD <ttnk.0112@gmail.com>
2020-03-09 22:03:53 +0900

    initial commit

---
+++ Assets/Scripts/artillery.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class artillery : MonoBehaviour
+{
+    public float shootPower = 100;
+    public GameObject projectilePrefab;
+    //private Rigidbody projectileRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //projectileRb = projectilePrefab.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+        projectileRb.AddForce(projectileRb.transform.forward * shootPower, ForceMode.Impulse);
+        
+    }
+}
